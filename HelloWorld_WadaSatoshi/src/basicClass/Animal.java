a52895892e5706d0bebf2fa7c528a3ab673b63a0
wata0505 <wata0505@DESKTOP-9MOEO9L>
2025-04-24 01:21:21 +0900

    basicClass

--- HelloWorld_WadaSatoshi/src/basicClass/Animal.java
+++ HelloWorld_WadaSatoshi/src/basicClass/Animal.java
@@ -7,11 +7,11 @@ public class Animal {
 	
 	public static void main(String[] args) {
 		  // Q1：Dogクラスを呼び出して変数に代入し、動物の名前をコンソールに出力してください。
-        Dog dog1 = new Dog();  // 名前だけのコンストラクタを使う
+        Dog dog1 = new Dog();
         System.out.println("動物の名前: " + dog1.name);
 
         // Q2：Dogクラスを呼び出して変数に代入し、動物の数をコンソールに出力してください。
-        Dog dog2 = new Dog(3);  // 数を渡すコンストラクタを使う
+        Dog dog2 = new Dog(3);
         System.out.println("動物の数: " + dog2.count);
 
         // 現在の日時を「yyyy-MM-dd H:m:s」形式で変数に代入して、コンソールに出力してください
