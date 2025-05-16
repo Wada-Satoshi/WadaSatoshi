81d71164522be640c20d3da6825d5a0781c328b3
wata0505 <wata0505@DESKTOP-9MOEO9L>
2025-05-16 14:19:57 +0900

    introduction

--- HelloWorld_WadaSatoshi/src/introduction/Person.java
+++ HelloWorld_WadaSatoshi/src/introduction/Person.java
@@ -1,11 +1,9 @@
 package introduction;
-
 public class Person {
     String name;
     int age;
     double height;
     double weight;
-
     static int count = 0;
 
     public Person(String name, int age, double height, double weight) {
@@ -17,17 +15,19 @@ public class Person {
     }
 
     public double bmi() {
-        return this.weight / (this.height * this.height);
+        double bmi = this.weight / (this.height * this.height);
+        double flooredBmi = Math.floor(bmi); 
+        return flooredBmi;
     }
 
     public void print() {
         System.out.println("名前は" + this.name + "です");
         System.out.println("年は" + this.age + "才です");
-        System.out.println(String.format("BMIは%.1fです\n", this.bmi()));
+        System.out.println("BMIは" + this.bmi() + "です");
+        System.out.println();
     }
 
-    public static int getCount() {
-        return count;
+    public static void printCount() {
+        System.out.println("合計" + count + "人です");
     }
 }
-
